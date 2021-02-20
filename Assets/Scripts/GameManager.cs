114c207de8499b5d93b8cfce0f4de30b447b8564
Keylor-Rosales <Keylorrosales@hotmail.com>
2021-02-20 04:24:28 -0600

    Subiendo Archivos

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+	bool gameHasEnded = false;
+	public float restartDelay = 1f;
+	public GameObject CompleteLevelUI;
+	public GameObject DesactiveUI;
+	public int nextSceneLoad;
+
+	void Start()
+	{
+		nextSceneLoad = SceneManager.GetActiveScene().buildIndex + 1;
+	}
+
+	public void CompleteLevel()
+	{
+		CompleteLevelUI.SetActive(true);
+		DesactiveUI.SetActive(false);
+	}
+
+	public void EndGame()
+	{
+		if (gameHasEnded == false)
+		{
+			gameHasEnded = true;
+			Invoke("Restart", restartDelay);
+		}
+	}
+
+	void Restart()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+}
---
+++ Assets/Scripts/ScriptTime.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptTime : MonoBehaviour
+{
+    public GameObject[] ObjectActiveTime;
+    public float InicioActive;
+    public float Fin;
+
+    void Update()
+    {
+        InicioActive += Time.deltaTime;
+        for (int i = 0; i < ObjectActiveTime.Length; i++)
+        {
+            if (InicioActive >= Fin)
+            {
+                ObjectActiveTime[i].SetActive(true);
+                InicioActive = 0;
+                this.enabled = false;
+            }
+        }
+    }
+}
